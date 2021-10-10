2908a32a847330f6f34eb75ee9c017150250e32e
ivanzabalm <ivanzabalm@gmail.com>
2021-10-10 11:16:12 +0200

    funcion set realizada en clase Persona e implementacion de  personas y personasJSON

--- P1_GX_Alvaro_Jimenez-Ivan_Zabaleta.py
+++ P1_GX_Alvaro_Jimenez-Ivan_Zabaleta.py
@@ -33,17 +33,15 @@ class ModelCursor:
             documento que se itera.
             command_cursor (CommandCursor) -- Cursor de pymongo
         """
-        #TODO
-
-
+        
+        # command_cursor = CommandCursor()
         pass 
     
     def next(self):
         """ Devuelve el siguiente documento en forma de modelo
         """
-        # TODO
-        # command_cursor.next()
         
+        # command_cursor.next()
         pass 
 
     @property
@@ -74,9 +72,8 @@ class Persona:
         pass
 
     def set(self, **kwargs):
+        self.__dict__.update(kwargs)
 
-        pass 
-    
     @classmethod
     def find(cls, query):
         """ Devuelve un cursor de modelos        
@@ -101,12 +98,84 @@ class Persona:
         cls.admissible_vars = cls.admissible_vars[0][0].split(',')
 
         # funcion map para deshacerse del '\n' de la primera linea 
-        cls.required_vars = list(map(str.strip,cls.required_vars))
-        print(cls.required_vars)
-        print(cls.admissible_vars)        
+        cls.required_vars = list(map(str.strip,cls.required_vars))      
 
-# Q1: Listado de todas las compras de un cliente
-nombre = "Definir"
+personas = []
+personasJSON = [
+    {
+        "nombre": "Julian",
+        "apellido": "Fernandez",
+        "telefono" : 674165642,
+        "ciudad" : "Paris",
+        "estudios": {"universidad": "UPM", "inicio": "19/04/2012", "final": "24/03/2005"}
+    },
+    {
+        "nombre": "Sergio",
+        "apellido": "Diaz",
+        "telefono" : 619421988,
+        "ciudad" : "Huelva",
+        "estudios": {"universidad": "UAM", "inicio": "23/05/2001", "final": "24/08/2005"}
+    },
+    {
+        "nombre": "Javier",
+        "apellido": "Cortes",
+        "telefono" : 669197778,
+        "ciudad" : "Santander",
+        "estudios": {"universidad": "U-tad", "inicio": "30/08/2000", "final": "09/11/2010"}
+    },
+    {
+        "nombre": "Carmen",
+        "apellido": "Cortes",
+        "telefono" : 629755914,
+        "ciudad" : "Barcelona",
+        "estudios": {"universidad": "UPM", "inicio": "19/04/2012", "final": "24/03/2005"}
+    },
+    {
+        "nombre": "Alberto",
+        "apellido": "Rayo",
+        "telefono" : 660706957,
+        "ciudad" : "Santander",
+        "estudios": {"universidad": "UAM", "inicio": "19/04/2012", "final": "24/03/2005"}
+
+    },
+    {
+        "nombre": "Pedro",
+        "apellido": "Diéguez",
+        "telefono" : 660706957,
+        "ciudad" : "Santander",
+        "estudios": {"universidad": "UAM", "inicio": "19/04/2012", "final": "24/03/2005"}
+    },
+    {
+        "nombre": "Alberto",
+        "apellido": "Rayo",
+        "telefono" : 609304424,
+        "ciudad" : "Sevilla",
+        "universidad" : "UAM",
+        "estudios": {"universidad": "UPM", "inicio": "02/06/2002", "final": "06/11/2016"}
+    },
+    {
+        "nombre": "José",
+        "apellido": "Hernández",
+        "telefono" : 693401628,
+        "ciudad" : "Santander",
+        "estudios": {"universidad": "UAM", "inicio": "12/08/2009", "final": "18/07/2011"}
+    },
+    {
+        "nombre": "Teresa",
+        "apellido": "Colmenero",
+        "telefono" : 691999884,
+        "ciudad" : "Bilbao",
+        "estudios": {"universidad": "UAM", "inicio": "30/08/2017", "final": "15/03/2001"}
+    },
+    {
+        "nombre": "Luis",
+        "apellido": "Izquierdo",
+        "telefono" : 691999884,
+        "ciudad" : "Bilbao",
+        "estudios": {"universidad": "UAM", "inicio": "30/08/2017", "final": "15/03/2001"}
+    }
+]
+
 Q1 = []
 Q2 = []
 Q3 = []
@@ -118,8 +187,22 @@ Q7 = []
 if __name__ == '__main__':
     client = MongoClient()
     db = client.test.personas
-    persona = Persona()
-    persona.init_class(db,"vars.txt")
-    persona.set()
     
-    
+    p1 = Persona()
+    p1.init_class(db,"vars.txt")
+    p1.set(**personasJSON[0])
+
+    p2 = Persona()
+    p2.init_class(db,"vars.txt")
+    p2.set(**personasJSON[1])
+
+    p3 = Persona()
+    p3.init_class(db,"vars.txt")
+    p3.set(**personasJSON[2])
+
+    personas.append(p1)
+    personas.append(p2)
+    personas.append(p3)
+
+    for obj in personas:
+        print(obj.__dict__)
