0b45ce41b0d6a97c192831a0a5330d80f24fe1db
alvaro <Jimenez1212>
2021-10-18 17:47:30 +0200

    clase empresa y centro, JSONS de cada uno, vars.txt de cada uno y bucle en el main para insertar losJSON. He comentado lo del geoJSON por que no me va en mi ordenador

--- P1_GX_Alvaro_Jimenez-Ivan_Zabaleta.py
+++ P1_GX_Alvaro_Jimenez-Ivan_Zabaleta.py
@@ -143,6 +143,202 @@ class Persona:
         cls.required_vars.extend(list(set(listReqVars) - set(cls.required_vars)))
         cls.admissible_vars.extend(list(set(listAdmVars) - set(cls.admissible_vars)))
 
+class Empresa:
+    required_vars = []
+    admissible_vars = []
+    db = None
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def save(self):
+        qr = {"ID":self.__dict__.get('ID')} # identificar
+        qemp = self.db.find(qr,{"_id":0}) # no salga el id
+
+        for empresa in qemp:
+            ins = {} # Se guardan las que se van a actualizar.
+            l1 = set(empresa.keys())
+            l2 = set(self.__dict__.keys())
+            resl = list(sorted(l2-l1)) # Busca las que se necesitan actualizar.
+
+            if len(resl) > 0:
+                for p in resl:
+                    ins[p] = self.__dict__.get(p) # Coge las que necesita actualizar.
+            
+            for k in empresa.keys():
+                if self.__dict__.get(k) != empresa[k]:
+                    ins[k] = self.__dict__.get(k) # Cambia las que necesitan actualizar.
+            
+            db.update_one(qr,{"$set":ins})
+
+        if qemp.count() == 0:
+            ert = {}
+            for we in self.__dict__.keys():
+                ert[we] = self.__dict__.get(we)
+            db.insert_one(ert) # Si no existe introduce uno nuevo.
+
+    def set(self, **kwargs):
+        valido = True
+
+        # Comprobacion de variables requeridas.
+        for n in self.required_vars:
+            if not (kwargs.get(n)):
+                valido = False
+                print("Error: el documento no contiene las variables requeridas.")
+
+        # Comprobacion de variables admitidas.
+        for n in kwargs:
+            if not n in self.required_vars:
+                if not n in self.admissible_vars:
+                    valido = False
+                    print("Error: el documento contiene una o varias variables no admitidas.")
+                
+        # En caso de que las comprobaciones sean exitosas se almacenara en el diccionario de la clase.
+        if(valido):
+            if not(bool(self.__dict__)):
+                self.__dict__.update(kwargs)
+            else:
+                if(self.__dict__ != kwargs):
+                    for n in kwargs:
+                        if n in self.__dict__:
+                            if(self.__dict__[n] != kwargs[n]):
+                                self.__dict__[n] = kwargs[n]
+                        else:
+                            self.__dict__[n] = kwargs[n]
+
+            # Añadimos el geoJson de tipo punto al documento
+            #self.__dict__["geolocalizacion"] = getCityGeoJSON(self.__dict__.get("ciudad"))
+
+    @classmethod
+    def find(cls, query):
+        """ Devuelve un cursor de modelos        
+        """ 
+        return ModelCursor(cls,cls.db.find(query))
+
+    @classmethod
+    def init_class(cls, db, vars_path="vars-empresa.txt"):
+        """ Inicializa las variables de clase en la inicializacion del sistema.
+        Argumentos:
+            db (MongoClient) -- Conexion a la base de datos.
+            vars_path (str) -- ruta al archivo con la definicion de variables
+            del modelo.
+        """
+        cls.db = db
+
+        with open(vars_path, "r") as f:
+            listReqVars = f.readlines(1)
+            listAdmVars = f.readlines(1)
+
+        # Para deshacerse de los "\n" y reformar la lista con cada una de las variables ordenadas.
+        listReqVars = list(map(str.strip,listReqVars))[0].split(",")
+        listAdmVars = list(map(str.strip,listAdmVars))[0].split(",")
+
+        # Aplicando la diferencia de la teoria de conjuntos (B-A) le aplicamos las nuevas variables.
+        cls.required_vars.extend(list(set(listReqVars) - set(cls.required_vars)))
+        cls.admissible_vars.extend(list(set(listAdmVars) - set(cls.admissible_vars)))
+
+
+
+class Centro:
+    required_vars = []
+    admissible_vars = []
+    db = None
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def save(self):
+        qr = {"ID":self.__dict__.get('ID')} # identificar
+        qcen = self.db.find(qr,{"_id":0}) # no salga el id
+
+        for empresa in qcen:
+            ins = {} # Se guardan las que se van a actualizar.
+            l1 = set(empresa.keys())
+            l2 = set(self.__dict__.keys())
+            resl = list(sorted(l2-l1)) # Busca las que se necesitan actualizar.
+
+            if len(resl) > 0:
+                for p in resl:
+                    ins[p] = self.__dict__.get(p) # Coge las que necesita actualizar.
+            
+            for k in empresa.keys():
+                if self.__dict__.get(k) != empresa[k]:
+                    ins[k] = self.__dict__.get(k) # Cambia las que necesitan actualizar.
+            
+            db.update_one(qr,{"$set":ins})
+
+        if qcen.count() == 0:
+            ert = {}
+            for we in self.__dict__.keys():
+                ert[we] = self.__dict__.get(we)
+            db.insert_one(ert) # Si no existe introduce uno nuevo.
+
+    def set(self, **kwargs):
+        valido = True
+
+        # Comprobacion de variables requeridas.
+        for n in self.required_vars:
+            if not (kwargs.get(n)):
+                valido = False
+                print("Error: el documento no contiene las variables requeridas.")
+
+        # Comprobacion de variables admitidas.
+        for n in kwargs:
+            if not n in self.required_vars:
+                if not n in self.admissible_vars:
+                    valido = False
+                    print("Error: el documento contiene una o varias variables no admitidas.")
+                
+        # En caso de que las comprobaciones sean exitosas se almacenara en el diccionario de la clase.
+        if(valido):
+            if not(bool(self.__dict__)):
+                self.__dict__.update(kwargs)
+            else:
+                if(self.__dict__ != kwargs):
+                    for n in kwargs:
+                        if n in self.__dict__:
+                            if(self.__dict__[n] != kwargs[n]):
+                                self.__dict__[n] = kwargs[n]
+                        else:
+                            self.__dict__[n] = kwargs[n]
+
+            # Añadimos el geoJson de tipo punto al documento
+            #self.__dict__["geolocalizacion"] = getCityGeoJSON(self.__dict__.get("ciudad"))
+
+    @classmethod
+    def find(cls, query):
+        """ Devuelve un cursor de modelos        
+        """ 
+        return ModelCursor(cls,cls.db.find(query))
+
+    @classmethod
+    def init_class(cls, db, vars_path="vars-centro_educativo.txt"):
+        """ Inicializa las variables de clase en la inicializacion del sistema.
+        Argumentos:
+            db (MongoClient) -- Conexion a la base de datos.
+            vars_path (str) -- ruta al archivo con la definicion de variables
+            del modelo.
+        """
+        cls.db = db
+
+        with open(vars_path, "r") as f:
+            listReqVars = f.readlines(1)
+            listAdmVars = f.readlines(1)
+
+        # Para deshacerse de los "\n" y reformar la lista con cada una de las variables ordenadas.
+        listReqVars = list(map(str.strip,listReqVars))[0].split(",")
+        listAdmVars = list(map(str.strip,listAdmVars))[0].split(",")
+
+        # Aplicando la diferencia de la teoria de conjuntos (B-A) le aplicamos las nuevas variables.
+        cls.required_vars.extend(list(set(listReqVars) - set(cls.required_vars)))
+        cls.admissible_vars.extend(list(set(listAdmVars) - set(cls.admissible_vars)))
+
+
+
+
+
+
+
 personasJSON = [
     {
         "nombre": "Julian",
@@ -217,6 +413,155 @@ personasJSON = [
     }
 ]
 
+
+empresasJSON = [
+    {
+        "ID": 1,
+        "nombre": "McDonalds",
+        "sector" : "Comida",
+        "ciudad" : "Madrid",
+        "ceo": "Sech"
+    },
+    {
+        "ID": 2,
+        "nombre": "Nike",
+        "sector" : "Ropa",
+        "ciudad" : "Barcelona",
+        "ceo": "Myke"
+    },
+    {
+        "ID": 3,
+        "nombre": "Adidas",
+        "sector" : "Ropa",
+        "ciudad" : "Alicante",
+        "ceo": "Ivan"
+    },
+    {
+        "ID": 4,
+        "nombre": "Apple",
+        "sector" : "Tecnologia",
+        "ciudad" : "Paris",
+        "ceo": "Steve"
+    },
+    {
+        "ID": 5,
+        "nombre": "Samsung",
+        "sector" : "Tecnologia",
+        "ciudad" : "Sevilla",
+        "ceo": "Xokas"
+
+    },
+    {
+        "ID": 6,
+        "nombre": "FIFA",
+        "sector" : "Deporte",
+        "ciudad" : "Londres",
+        "ceo": "Iniesta"
+    },
+    {
+        "ID": 7,
+        "nombre": "Burguer King",
+        "sector" : "Comida",
+        "ciudad" : "Valencia",
+        "ceo": "Gordo"
+    },
+    {
+        "ID": 8,
+        "nombre": "Riot Games",
+        "sector" : "Videojuegos",
+        "ciudad" : "Roma",
+        "ceo": "Teemo"
+    },
+    {
+        "ID": 9,
+        "nombre": "JBL",
+        "sector" : "Musica",
+        "ciudad" : "Amsterdam",
+        "ceo": "Stormzy"
+    },
+    {
+        "ID": 10,
+        "nombre": "BOSE",
+        "sector" : "Musica",
+        "ciudad" : "Brasil",
+        "ceo": "Miguel"
+    }
+]
+
+centrosJSON = [
+    {
+        "ID": 1,
+        "universidad": "U-tad",
+        "carrera" : "INSO",
+        "ciudad" : "Madrid",
+        "alumnos": 15890
+    },
+    {
+        "ID": 2,
+        "universidad": "CEU",
+        "carrera" : "ADE",
+        "ciudad" : "Madrid",
+        "alumnos": 32341
+    },
+    {
+        "ID": 3,
+        "universidad": "UA",
+        "carrera" : "Derecho",
+        "ciudad" : "Alicante",
+        "alumnos": 20000
+    },
+    {
+        "ID": 4,
+        "universidad": "UV",
+        "carrera" : "Arquitectura",
+        "ciudad" : "Valencia",
+        "alumnos": 64483
+    },
+    {
+        "ID": 5,
+        "universidad": "Complutense",
+        "carrera" : "Arte y Diseño",
+        "ciudad" : "Madrid",
+        "alumnos": 87832
+
+    },
+    {
+        "ID": 6,
+        "universidad": "Universidad de Salamanca",
+        "carrera" : "Historia",
+        "ciudad" : "Salamnca",
+        "alumnos": 9500
+    },
+    {
+        "ID": 7,
+        "universidad": "ICADE",
+        "carrera" : "Economia",
+        "ciudad" : "Madrid",
+        "alumnos": 35000
+    },
+    {
+        "ID": 8,
+        "universidad": "UPF",
+        "carrera" : "Bioquimica",
+        "ciudad" : "Barcelona",
+        "alumnos": 3400
+    },
+    {
+        "ID": 9,
+        "universidad": "Oxford",
+        "carrera" : "Bussines",
+        "ciudad" : "Londres",
+        "alumnos": 40000
+    },
+    {
+        "ID": 10,
+        "universidad": "Harvard",
+        "carrera" : "Law ",
+        "ciudad" : "Cambridge",
+        "alumnos": 40000
+    }
+]
+
 Q1 = []
 Q2 = []
 Q3 = []
@@ -227,19 +572,46 @@ Q7 = []
 
 if __name__ == '__main__':
     client = MongoClient()
-    db = client.test.personas
-    listaPersonas = [Persona() for i in range(10)]
-   
+    
+    # db = client.test.personas
+    # listaPersonas = [Persona() for i in range(10)]
+    # i=0
+    # for n in listaPersonas:
+    #     n.init_class(db,"vars-persona.txt")
+    #     n.set(**personasJSON[i])
+    #     n.save()
+    #     time.sleep(0.5)
+    #     i += 1
+    
+    # db = client.test.empresas
+    # listaEmpresas = [Empresa() for i in range(10)]
+    # i=0
+    # for n in listaEmpresas:
+    #     n.init_class(db,"vars-empresa.txt")
+    #     n.set(**empresasJSON[i])
+    #     n.save()
+    #     time.sleep(0.5)
+    #     i += 1
+
+    db = client.test.centrosEd
+    listaCentros = [Centro() for i in range(10)]
     i=0
-    for n in listaPersonas:
-        n.init_class(db,"vars-persona.txt")
-        n.set(**personasJSON[i])
+    for n in listaCentros:
+        n.init_class(db,"vars-centro_educativo.txt")
+        n.set(**centrosJSON[i])
         n.save()
         time.sleep(0.5)
         i += 1
     
-    Q1 = db.find({"ciudad": "Huelva"})
 
-    print("Resultado de la Q1:")
-    for x in Q1:
-        print(x)  
+
+
+
+
+
+
+    # Q1 = db.find({"ciudad": "Huelva"})
+
+    # print("Resultado de la Q1:")
+    # for x in Q1:
+    #     print(x) 
